9a5c0f81ea7e22850c53ae3e54ad3c31b53b1a6c
igonzaleztb <ignacio.gonzalez@thebridgeschool.es>
2023-10-17 19:31:13 +0200

    update desing

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,17 +1,20 @@
-import { useState, useEffect } from "react";
-import EN from "../EN.json"
-import ES from "../ES.json"
+import { useState, useEffect, useRef } from "react";
+import EN from "../EN.json";
+import ES from "../ES.json";
 import alergenos from "../alergenos/alerjenos.json";
 import logo from "../assets/logo3.png";
 import "../App.css";
+import up from "../assets/up.png";
 
-function Menu({intl}) {
+function Menu({ intl }) {
+  const [isVisible, setIsVisible] = useState(false);
   const [count, setCount] = useState(0);
-  const [carta,setCarta] = useState("")
+  const [carta, setCarta] = useState("");
   const [viewAddText, setViewAddText] = useState(false);
   const [typesProds, setTypesProds] = useState("");
-
- 
+  const mainRef = useRef(null);
+  const [scrollTo, setScrollTo] = useState("none");
+  const prevScrollPos = useRef(0);
   function addZeroes(num) {
     return num.toLocaleString("en", {
       useGrouping: false,
@@ -19,6 +22,35 @@ function Menu({intl}) {
     });
   }
 
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      const currentScrollPos = window.pageYOffset;
+ 
+
+      if(currentScrollPos!==0){
+        setIsVisible(true);
+      }else{
+        setIsVisible(false);
+      }
+      
+      // Button is displayed after scrolling for 500 pixels
+      
+        
+
+      prevScrollPos.current = currentScrollPos;
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, [isVisible]);
   const capitalizeWord = (text) => {
     const lower = text.toLowerCase();
 
@@ -30,27 +62,21 @@ function Menu({intl}) {
   }
 
   useEffect(() => {
-    if(carta){
+    if (carta) {
+      const tipos = carta.map((typ) => typ.TYPE);
 
+      let uniqueChars = [...new Set(tipos)];
 
-        const tipos = carta.map((typ) => typ.TYPE);
-
-        let uniqueChars = [...new Set(tipos)];
-    
-        setTypesProds(uniqueChars);
+      setTypesProds(uniqueChars);
     }
- 
   }, [carta]);
 
+ 
   useEffect(() => {
-   
-    
-    if(intl){
-
-        intl ==='ES'? setCarta(ES) :setCarta(EN)
+    if (intl) {
+      intl === "ES" ? setCarta(ES) : setCarta(EN);
     }
-  }, [intl])
-  
+  }, [intl]);
 
   const deleteDuplicates = (array) => {
     var uniq = {};
@@ -73,8 +99,7 @@ function Menu({intl}) {
         arrayAlergenos.push(filtrado);
       }
 
-      console.log(arrayAlergenos);
-
+ 
       return arrayAlergenos.map((alerg) => {
         return <img className="alergenos-icon" src={alerg.PIC} />;
       });
@@ -84,68 +109,82 @@ function Menu({intl}) {
   const additionalText = () => {};
   return (
     <div className="flip-scale-up-hor">
-      <img src={logo} className="logo" />
-      <table>
+      <img src={logo} className="logo" /> 
+      <div id="home">
+        {typesProds &&
+          typesProds.map((prod) => (
+            <button className="button" >
+              <a href={`#${prod}`}>{prod}</a>
+            </button>
+          ))}
+      </div>
+      <a href="#">
+        {" "}
+
+        {isVisible &&  <img src={up} onClick={()=>{scrollToTop }}  className="stickybtn"   />} 
+       
+      </a>
+
+      <table ref={mainRef}>
         <tr className="separator">
-        {typesProds
-          ? typesProds.map((prods, i) => {
-              return (
-                <>
-                  <tr key={i}>
-                    <td>
-                      <h1>{prods}</h1>
-                    </td>
-                  </tr>
-                  {carta
-                    ? carta
-                        .filter((cart) => cart.TYPE === prods)
-                        .map((prod, i) => {
-                          const tipo = prod.TYPE;
-                          console.log(prod.DESCRIPTION);
-                          return (
-                            <tr key={i} className="tr-menu">
-                              {!prod.DESCRIPTION ? (
-                                <td className={prod.STYLE ? prod.STYLE : ""}>
-                                  {tipo === "PLATOS COMBINADOS"
-                                    ? `${i + 1}. `
-                                    : ""}
-                                  {capitalizeWord(prod.NAME)}
-                                  {searchAlergenos(prod.ALERG)}
-                                </td>
-                              ) : (
-                                <td>
-                                  {tipo === "PLATOS COMBINADOS"
-                                    ? `${i + 1}. `
-                                    : ""}
-                                  {capitalizeWord(prod.NAME)}
-                                  {searchAlergenos(prod.ALERG)}
-                                  <span>
-                                    {" "}
-                                    {capitalizeWord(prod.DESCRIPTION)}
-                                  </span>
-                                </td>
-                              )}
+          {typesProds
+            ? typesProds.map((prods, i) => {
+                return (
+                  <>
+                    <tr key={i} id={prods}>
+                      <td>
+                        <h1>{prods}</h1>
+                      </td>
+                    </tr>
+                    {carta
+                      ? carta
+                          .filter((cart) => cart.TYPE === prods)
+                          .map((prod, i) => {
+                            const tipo = prod.TYPE;
+                            return (
+                              <tr key={i} className="tr-menu">
+                                {!prod.DESCRIPTION ? (
+                                  <td className={prod.STYLE ? prod.STYLE : ""}>
+                                    {tipo === "PLATOS COMBINADOS"
+                                      ? `${i + 1}. `
+                                      : ""}
+                                    {capitalizeWord(prod.NAME)}
+                                    {searchAlergenos(prod.ALERG)}
+                                  </td>
+                                ) : (
+                                  <td>
+                                    {tipo === "PLATOS COMBINADOS"
+                                      ? `${i + 1}. `
+                                      : ""}
+                                    {capitalizeWord(prod.NAME)}
+                                    {searchAlergenos(prod.ALERG)}
+                                    <span>
+                                      {" "}
+                                      {capitalizeWord(prod.DESCRIPTION)}
+                                    </span>
+                                  </td>
+                                )}
 
-                              <td></td>
-                              <td className="price">
-                                {addZeroes(prod.PRICE)}
-                                {prod.PRICE ? "€" : ""}
-                              </td>
-                            </tr>
-                          );
-                        })
-                    : ""}
+                                <td></td>
+                                <td className="price">
+                                  {addZeroes(prod.PRICE)}
+                                  {prod.PRICE ? "€" : ""}
+                                </td>
+                              </tr>
+                            );
+                          })
+                      : ""}
 
-                  <tr key={i}>
-                    <td>
-                      <h1> {prods.TYPE === "INFO_ADD" ? prods.NAME : ""}</h1>
-                    </td>
-                  </tr>
-                </>
-              );
-            })
-          : ""}
-          </tr>
+                    <tr key={i}>
+                      <td>
+                        <h1> {prods.TYPE === "INFO_ADD" ? prods.NAME : ""}</h1>
+                      </td>
+                    </tr>
+                  </>
+                );
+              })
+            : ""}
+        </tr>
       </table>
     </div>
   );
